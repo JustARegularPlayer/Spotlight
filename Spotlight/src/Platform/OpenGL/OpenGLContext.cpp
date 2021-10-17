0f23c124c67fe6dbb66a10a1e0ed6804e4909ec7
Nathan Gavieres <justaregularplayer000@gmail.com>
2021-10-17 19:07:43 +0800

    Implemented OpenGL error callback system

--- Spotlight/src/Platform/OpenGL/OpenGLContext.cpp
+++ Spotlight/src/Platform/OpenGL/OpenGLContext.cpp
@@ -7,6 +7,53 @@
 namespace Spotlight
 {
 
+	static const char *GLErrorToString(GLenum error)
+	{
+		switch (error)
+		{
+			case GL_DEBUG_SOURCE_API			   :  return "GL_DEBUG_SOURCE_API";				  break;
+			case GL_DEBUG_SOURCE_WINDOW_SYSTEM	   :  return "GL_DEBUG_SOURCE_WINDOW_SYSTEM";     break;
+			case GL_DEBUG_SOURCE_THIRD_PARTY	   :  return "GL_DEBUG_SOURCE_THIRD_PARTY";       break;
+			case GL_DEBUG_SOURCE_APPLICATION	   :  return "GL_DEBUG_SOURCE_APPLICATION";       break;
+			case GL_DEBUG_SOURCE_OTHER			   :  return "GL_DEBUG_SOURCE_OTHER";             break;
+			case GL_DEBUG_TYPE_ERROR			   :  return "GL_DEBUG_TYPE_ERROR";               break;
+			case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR :  return "GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR"; break;
+			case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR  :  return "GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR";  break;
+			case GL_DEBUG_TYPE_PORTABILITY		   :  return "GL_DEBUG_TYPE_PORTABILITY";         break;
+			case GL_DEBUG_TYPE_PERFORMANCE		   :  return "GL_DEBUG_TYPE_PERFORMANCE";         break;
+			case GL_DEBUG_TYPE_MARKER			   :  return "GL_DEBUG_TYPE_MARKER";              break;
+			case GL_DEBUG_TYPE_PUSH_GROUP		   :  return "GL_DEBUG_TYPE_PUSH_GROUP";          break;
+			case GL_DEBUG_TYPE_POP_GROUP		   :  return "GL_DEBUG_TYPE_POP_GROUP";           break;
+			case GL_DEBUG_TYPE_OTHER			   :  return "GL_DEBUG_TYPE_OTHER";               break;
+			case GL_DEBUG_SEVERITY_HIGH			   :  return "GL_DEBUG_SEVERITY_HIGH";            break;
+			case GL_DEBUG_SEVERITY_MEDIUM		   :  return "GL_DEBUG_SEVERITY_MEDIUM";          break;
+			case GL_DEBUG_SEVERITY_LOW			   :  return "GL_DEBUG_SEVERITY_LOW";             break;
+			case GL_DEBUG_SEVERITY_NOTIFICATION	   :  return "GL_DEBUG_SEVERITY_NOTIFICATION";    break;
+			default: return "GL_DONT_CARE";
+		}
+	}
+
+	static void APIENTRY OnError(GLenum source,
+								 GLenum type,
+								 GLuint id,
+								 GLenum severity,
+								 GLsizei length,
+								 const GLchar *message,
+								 const void *userParam)
+	{
+		if(type == GL_DEBUG_TYPE_ERROR)
+			SPL_CORE_FATAL("OpenGL Error! :");
+
+		SPL_CORE_FATAL("Source = {}", GLErrorToString(source));
+		SPL_CORE_FATAL("Type = {}", GLErrorToString(type));
+		SPL_CORE_FATAL("ID = {}", id);
+		SPL_CORE_FATAL("Severity = {}", GLErrorToString(severity));
+		SPL_CORE_FATAL("Message:\n {}", message);
+
+		if(severity == GL_DEBUG_SEVERITY_HIGH)
+			SPL_CORE_ASSERT(false, '\0');
+	}
+		
 	OpenGLContext::OpenGLContext(GLFWwindow* handle)
 		: m_Handle(handle)
 	{
@@ -20,6 +67,10 @@ namespace Spotlight
 		glfwMakeContextCurrent(m_Handle);
 		int success = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
 		SPL_CORE_ASSERT(success, "Could not initialize Glad!");
+
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(OnError, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, true);
 
 		SPL_CORE_INFO("OpenGL Driver Details:");
 		SPL_CORE_INFO("\t- Vendor: {}", glGetString(GL_VENDOR));
